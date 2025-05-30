32caba3da2e43416db5bb749fb9b66c4c00b123a
unknown <garmashkir09@mail.ru>
2025-05-30 15:24:56 +0300

    начал делать бэк-энд с использованием Flask

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    return render_template('registration.html')
+
+@app.route('/home')
+def home():
+    return render_template('home_page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
