6a6d3623c87d4a698e567bf961114a3ce4ba4ed4
unknown <garmashkir09@mail.ru>
2025-06-16 20:52:52 +0300

    изменил регестрацию

--- static/js/register.js
+++ static/js/register.js
@@ -1,11 +1,14 @@
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const toSignIn = document.getElementById('toSignIn');
+const toSignUp = document.getElementById('toSignUp');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+if (toSignIn) {
+    toSignIn.addEventListener('click', () => {
+        container.classList.add('show-signin');
+    });
+}
+if (toSignUp) {
+    toSignUp.addEventListener('click', () => {
+        container.classList.remove('show-signin');
+    });
+}
